c4d49710d02d4c11ac55d86fa8fd6fd448a8bcae
FRANCO8724 <Arrigoni.20179@E2-9-PC08>
2024-01-23 13:21:05 +0100

    .

--- 14 - Esercitazione test con composite/Form1.cs
+++ 14 - Esercitazione test con composite/Form1.cs
@@ -10,7 +10,20 @@ using System.Windows.Forms;
 
 namespace _14___Esercitazione_test_con_composite
 {
+    public partial class Form1: Form
+    {
 
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+    }
 
 
 }
